f7477450be672305c6a7efa0b336abe06ba722da
Alina <simalina554@gmail.com>
2023-05-30 21:33:51 +0300

    семинар 4

---
+++ sem4_5/Program.cs
@@ -0,0 +1,20 @@
+﻿//30 Напишите программу, которая выводит массив из 8 элементов, 
+//заполненный нулями и единицами в случайном порядке.
+Console.Clear();
+Console.Write("num: ");
+int a = int.Parse(Console.ReadLine());
+int[] GetArr(int n)
+{
+    int[] arr = new int[a];
+    for (int i = 0; i < a; i++)
+    {
+        arr[i] = new Random().Next(0, 2);
+    }
+    return arr;
+}
+int [] arr =  GetArr(a);
+for (int i = 0; i < a; i++)
+{
+    Console.Write($"{arr[i]}");
+    //Console.WriteLine($"[{String.Join("; ---", array)}]");
+}
