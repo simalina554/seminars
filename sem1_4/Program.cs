69476bcd44c3463fab1e67d1e9e1d16799bd2dad
Alina <simalina554@gmail.com>
2023-06-04 18:53:07 +0300

    add sem1_4

---
+++ sem1_4/Program.cs
@@ -0,0 +1,11 @@
+﻿//6
+Console.Clear();
+Console.Write("Number : ");
+int a = int.Parse(Console.ReadLine());
+int s = a%2;
+if (s==0){
+    Console.WriteLine("чётное");
+}
+else{
+    Console.WriteLine("не чётное");
+}
