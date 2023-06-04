2f06b38da065365f22c091a41e961a46e033828d
Alina <simalina554@gmail.com>
2023-06-04 19:36:00 +0300

    add

---
+++ hw_sem4_27/Program.cs
@@ -0,0 +1,18 @@
+﻿/*
+Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+452 -> 11
+82 -> 10
+9012 -> 12
+*/
+int res = 0;
+int GetKol(int n){
+    while(n > 0){
+        res = res + n % 10;
+        n = n /10;
+    }
+    return res;
+}
+Console.Clear();
+Console.Write("num: ");
+int num = int.Parse(Console.ReadLine());
+Console.WriteLine($"{num} - {GetKol(num)}");
