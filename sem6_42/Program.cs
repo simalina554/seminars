16b007b34115bcb8458db28969cbcbf8524a48a3
Alina <simalina554@gmail.com>
2023-06-22 20:09:01 +0300

    new

---
+++ sem6_42/Program.cs
@@ -0,0 +1,16 @@
+﻿// **Задача 42:**Напишите программу,
+// которая будет преобразовывать десятичное число в двоичное.
+// 45 -> 101101
+// 3 -> 11
+// 2 -> 10
+Console.WriteLine(" " );
+int num = int.Parse(Console.ReadLine());
+Console.Write(Decimal(num));
+string Decimal(int n){
+    string binar = "";
+    while(n > 0){
+        binar = Convert.ToString(n % 2)+ binar;
+        n = n / 2;
+    }
+    return binar;
+}
