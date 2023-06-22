cc6b3bf0f53139880680423f93703bf7f55e9d2b
Alina <simalina554@gmail.com>
2023-06-22 20:06:48 +0300

    new

---
+++ seminar3/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.Clear();
+Console.Write("X: ");
+int X = int.Parse(Console.ReadLine());
+Console.Write("Y: ");
+int Y = int.Parse(Console.ReadLine());
+if(X>0 && Y>0){
+    Console.WriteLine("1");
+} else if(X < 0 && Y>0){
+    Console.WriteLine("2");
+}else if(X < 0 && Y < 0){
+    Console.WriteLine("3");
+}else if(X > 0 && Y < 0){
+    Console.WriteLine("4");
+}else if(X==0 || Y==0){
+    Console.WriteLine("На оси");
+}
