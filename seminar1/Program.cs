c267eaa023d1c52c2ff4977bf7d82904084ede51
Alina <simalina554@gmail.com>
2023-06-22 20:07:23 +0300

    new

---
+++ seminar1/Program.cs
@@ -0,0 +1,12 @@
+﻿//2
+Console.Write("Number 1: ");
+int a = int.Parse(Console.ReadLine());
+Console.Write("Number 2: ");
+int b = int.Parse(Console.ReadLine());
+if (a>b){
+    Console.WriteLine($"Максимальное число: {a}");
+}
+else{
+    Console.WriteLine($"Максимальное число: {b}");
+}
+
