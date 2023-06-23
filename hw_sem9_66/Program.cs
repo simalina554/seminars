0f2d4c6c986475e40927a6ba04835bcb1d19b47e
Alina <simalina554@gmail.com>
2023-06-23 17:25:23 +0300

    new

---
+++ hw_sem9_66/Program.cs
@@ -0,0 +1,27 @@
+﻿/*
+Задача 66: Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+M = 1; N = 15 -> 120
+M = 4; N = 8. -> 30
+*/
+
+using System;
+using static System.Console;
+
+Write("Введите M: ");
+int m = int.Parse(ReadLine());
+Write("Введите N: ");
+int n = int.Parse(ReadLine());
+
+
+int PrintNum(int m, int n){
+    int sum = 0;
+    if(m==n){
+        //WriteLine(m);
+        return m;
+    }
+    else{
+        sum =m + PrintNum(m+1, n);
+        return sum;
+    }
+}
+WriteLine(PrintNum(m, n));
